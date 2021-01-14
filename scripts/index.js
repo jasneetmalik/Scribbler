eea7ade9716afef73f5b391a47e596c688566c03
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-13 19:03:20 -0500

    Created SignIn Modal Form

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 var signUpModal = document.getElementById('signUpModal');
+var signInModal = document.getElementById('signInModal');
+var signInBtn = document.getElementById('signInBtn');
 var signUpBtn = document.getElementById('signUpBtn');
 var closeSignUp = document.getElementById('closeSignUp');
 
@@ -7,7 +9,19 @@ function showSignUpModal() {
     signUpModal.style.display = 'block';
 }
 
+function showSignInModal() {
+    signInModal.style.display = 'block';
+}
+
 function hideSignUpModal() {
     signUpModal.style.display = 'none';
 }
 
+function hideSignInModal() {
+    signInModal.style.display = 'none';
+}
+
+function redirect() {
+    hideSignInModal();
+    showSignUpModal();
+}
