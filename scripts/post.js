1b4ffbd78ad21c4cc5acd3bc1ceead23f2f602c2
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-16 16:09:33 -0500

    Added Toggle Edit/Save functionality

--- scripts/post.js
+++ scripts/post.js
@@ -2,3 +2,14 @@ var postid = window.location.hash.substring(1);
 window.onload = function () {
     document.getElementById(postid).style.display='flex';
 }
+function toggleEdit() {
+    var postDesc = document.getElementById('postDesc1');
+    var buttonText = document.getElementById('edit-save');
+    if (postDesc.isContentEditable) {
+        postDesc.contentEditable = 'false';
+        buttonText.innerHTML = 'Edit<i class="fas fa-edit"></i>';
+    } else {
+        postDesc.contentEditable = 'true';
+        buttonText.innerHTML = 'Save';
+    }
+}
