decdb9ef6151e29acf21c418257d8c374a6c289e
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-17 18:19:49 -0500

    Implemented All Comments functionality

--- scripts/post.js
+++ scripts/post.js
@@ -3,8 +3,8 @@ var postid = window.location.hash.substring(1);
 window.onload = function () {
     document.getElementById(postid).style.display='flex';
 }
-function toggleEdit() {
-    var postDesc = document.getElementById('postDesc1');
+function toggleEdit(postDescId) {
+    var postDesc = document.getElementById(postDescId);
     var buttonText = document.getElementById('edit-save');
     if (postDesc.isContentEditable) {
         postDesc.contentEditable = 'false';
