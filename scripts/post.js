9c26170b01b66811e2fc5014ee8f7b4196987f2e
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-16 22:25:34 -0500

    Implemented Like and Comment Functionality

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,4 @@
+var likeCount = 0;
 var postid = window.location.hash.substring(1);
 window.onload = function () {
     document.getElementById(postid).style.display='flex';
@@ -7,9 +8,22 @@ function toggleEdit() {
     var buttonText = document.getElementById('edit-save');
     if (postDesc.isContentEditable) {
         postDesc.contentEditable = 'false';
-        buttonText.innerHTML = 'Edit<i class="fas fa-edit"></i>';
+        buttonText.innerHTML = 'Edit <i class="fas fa-edit"></i>';
     } else {
         postDesc.contentEditable = 'true';
         buttonText.innerHTML = 'Save';
     }
+}
+
+function showLikeCount() {
+    likeCount++;
+    document.getElementById('noOfLikes').innerHTML = likeCount + " person likes this";
+    document.getElementById('likeButton').innerHTML = '<i class="fas fa-thumbs-up"></i> Liked';
+
+}
+function addComment() {
+    var allComments = document.getElementById('allComments');
+    var comment = document.getElementById('comment');
+    allComments.innerHTML = '<textarea readonly cols="110" rows="2" class="allComments">' + comment.value + '</textarea>' + allComments.innerHTML;
+    comment.value="";
 }
