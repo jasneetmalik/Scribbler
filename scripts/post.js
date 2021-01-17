7a685fcff152053632cdfe8b4e0ce1a7266edb2b
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-17 18:28:05 -0500

    Bug Fixes

--- scripts/post.js
+++ scripts/post.js
@@ -3,9 +3,9 @@ var postid = window.location.hash.substring(1);
 window.onload = function () {
     document.getElementById(postid).style.display='flex';
 }
-function toggleEdit(postDescId) {
+function toggleEdit(postDescId, editsave) {
     var postDesc = document.getElementById(postDescId);
-    var buttonText = document.getElementById('edit-save');
+    var buttonText = document.getElementById(editsave);
     if (postDesc.isContentEditable) {
         postDesc.contentEditable = 'false';
         buttonText.innerHTML = 'Edit <i class="fas fa-edit"></i>';
