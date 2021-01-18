da43e46f021166bd6216add101b3f101ed99dc18
jasneetmalik <jasneet_malik@hotmail.com>
2021-01-17 19:03:38 -0500

    Added Comments to Javascript files

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
+
+//Show Create Post Modal
 function showCreatePost() {
     document.getElementById('createPostModal').style.display = 'block';
 }
+
+//Hide Delete Post Modal
 function hideCreatePost() {
     document.getElementById('createPostModal').style.display = 'none';
 }
--- scripts/post.js
+++ scripts/post.js
@@ -1,26 +1,34 @@
 var likeCount = 0;
-var postid = window.location.hash.substring(1);
+
+//The Post ID is passed from PostsList so that only the post selected by user is displayed
+var postid = window.location.hash.substring(1); 
 window.onload = function () {
     document.getElementById(postid).style.display='flex';
 }
-function toggleEdit(postDescId, editsave) {
+
+//Function to toggle between Edit and Save for the selected post
+function toggleEdit(postDescId, editsave, titleId) {
     var postDesc = document.getElementById(postDescId);
     var buttonText = document.getElementById(editsave);
+    var title = document.getElementById(titleId);
     if (postDesc.isContentEditable) {
         postDesc.contentEditable = 'false';
+        title.contentEditable = 'false';
         buttonText.innerHTML = 'Edit <i class="fas fa-edit"></i>';
     } else {
         postDesc.contentEditable = 'true';
+        title.contentEditable = 'true';
         buttonText.innerHTML = 'Save';
     }
 }
-
+//Function to display the number of likes for the post
 function showLikeCount() {
     likeCount++;
     document.getElementById('noOfLikes').innerHTML = likeCount + " person likes this";
     document.getElementById('likeButton').innerHTML = '<i class="fas fa-thumbs-up"></i> Liked';
 
 }
+//Function to add a comment to the post
 function addComment() {
     var allComments = document.getElementById('allComments');
     var comment = document.getElementById('comment');
